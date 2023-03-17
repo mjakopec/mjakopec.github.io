5aa1f32cd4f93660d92b5af522f60b43e59ad534
dmestrovic <47329289+mestrovicd@users.noreply.github.com>
2023-03-17 22:51:43 +0100

    modified the gauge animation and separated it to a new css file (#4)
    Co-authored-by: meshkey <47329289+meshkey@users.noreply.github.com>

--- gauge/gauge.js
+++ gauge/gauge.js
@@ -1,64 +1,32 @@
 export class Gauge extends HTMLElement {
-    constructor() {
-      super();
-      this._trMin=this.getAttribute('min');
-      this._trMax=this.getAttribute('max');
-      this._trV=this.getAttribute('value');
-      this._trT=this.getAttribute('title');
-      this._trW=this.getAttribute('w');
-      this._trA = -90;
-      var slices = document.querySelector('slot');
-      this._slice = slices.innerHTML;
-      this.attachShadow({mode: 'open'});
-    }
+	constructor() {
+		super();
+		this._trMin = this.getAttribute("min");
+		this._trMax = this.getAttribute("max");
+		this._trV = this.getAttribute("value");
+		this._trT = this.getAttribute("title");
+		this._trW = this.getAttribute("w");
+		this._trA = -90;
+		var slices = document.querySelector("slot");
+		this._slice = slices.innerHTML;
+		this.attachShadow({ mode: "open" });
+	}
 
-    static get observedAttributes() {
-        return ['value'];
-      }
+	static get observedAttributes() {
+		return ["value"];
+	}
 
-    get style(){
-        return `<style>
+	get style() {
+		return `<style>
         @import url("style.css");
+        @import url("animations.css");
         :root {
-            --gauge-width: ${this._trW};
-            --min: ${this._trMin};
-            --max: ${this._trMax};
-            --actual-value: ${this._trV};
-          }
-          
-          @keyframes needleRotation {
-            0% {
-              transform: rotate(-90deg);
-            }
-            80% {
-              transform: rotate(
-                calc(
-                  -30deg + (var(--actual-value) - var(--min)) / (var(--max) - var(--min)) *
-                    180deg
-                )
-              );
-            }
-            100% {
-              transform: rotate(
-                calc(
-                  -50deg + (var(--actual-value) - var(--min)) / (var(--max) - var(--min)) *
-                    180deg
-                )
-              );
-            }
-          }
-          
-          @keyframes gaugeRotation {
-            0% {
-              transform: rotate(0);
-            }
-            100% {
-              transform: rotate(
-                calc((var(--trigg) - var(--min)) / (var(--max) - var(--min)) * 180deg)
-              );
-            }
-          }
-          
+          --gauge-width: ${this._trW};
+          --min: 0;
+          --max: ${this._trMax};
+          --actual-value: ${this._trV};
+        }
+
           .wrapper {
             display: inline-block;
             width: auto;
@@ -88,7 +56,7 @@ export class Gauge extends HTMLElement {
             width: var(--gauge-width);
             height: var(--gauge-height);
             transform-origin: calc(var(--gauge-width) / 2) var(--gauge-height);
-            animation: 1.5s ease-in 0s 1 gaugeRotation;
+            animation: 0.3s ease-in 0s 1 gaugeRotation;
             transform: rotate(
               calc((var(--trigg) - var(--min)) / (var(--max) - var(--min)) * 180deg)
             );
@@ -127,12 +95,12 @@ export class Gauge extends HTMLElement {
             position: absolute;
             bottom: 0;
             left: 50%;
-            height: calc(var(--gauge-height) / 1.1);
+            height: calc(var(--gauge-height) * 0.9);
             width: 2px;
             box-shadow: 0 2px 2px 1px rgba(0, 0, 0, 0.38);
             background: #15222e;
             transform-origin: bottom;
-            animation: 2s ease-in 0s 1 needleRotation;
+            animation: 2.5s ease 0s 1 needleRotation;
           }
           
           body {
@@ -140,10 +108,10 @@ export class Gauge extends HTMLElement {
             color: #fff;
           }
           
-        </style>`
-    }
-    get template(){
-        return `<div class="wrapper">
+        </style>`;
+	}
+	get template() {
+		return `<div class="wrapper">
         <div class="gauge" style="--gauge-width: ${this._trW}px; --min:${this._trMin}; --max: ${this._trMax}; --actual-value:${this._trV}">
           <div class="slices">
           ${this._slice}
@@ -154,28 +122,32 @@ export class Gauge extends HTMLElement {
             <div class="number">${this._trV}</div>
           </div>
         </div>
-      </div>`
-    }
-    render() {
-        this.shadowRoot.innerHTML = `${this.style}${this.template}`;
-    }
-    
-    calculation(){
-        this._trA = ((this._trV-this._trMin)/(this._trMax-this._trMin))*180-90;
-    }
+      </div>`;
+	}
+	render() {
+		this.shadowRoot.innerHTML = `${this.style}${this.template}`;
+	}
 
-    connectedCallback() {
-        this.render();  
-    }
-    
-    attributeChangedCallback(name, oldValue, newValue) {
-        if (name ==='value') {this._trV= newValue}
-        this.calculation();
-        this.render();
-    }
+	calculation() {
+		this._trA =
+			((this._trV - this._trMin) / (this._trMax - this._trMin)) * 180 -
+			90;
+	}
 
-    disconnectedCallback() {
-        console.log('callback disconnected')
-    }
+	connectedCallback() {
+		this.render();
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (name === "value") {
+			this._trV = newValue;
+		}
+		this.calculation();
+		this.render();
+	}
+
+	disconnectedCallback() {
+		console.log("callback disconnected");
+	}
 }
-customElements.define('my-gauge', Gauge);  
+customElements.define("my-gauge", Gauge);
